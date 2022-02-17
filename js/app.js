da82eb693f172f3ca862b9896512b9d5a5355984
Md-Mobassher <you@example.com>
2022-02-17 20:41:06 +0600

    final commit

--- js/app.js
+++ js/app.js
@@ -10,30 +10,81 @@ function getOutputValue (outputId, setValue){
     const outputValue = outputAmount.innerText;
     outputAmount.innerText = setValue;
 }
+ const errorMessage = document.getElementById('message');
 
- function getTotalCost( ){
+ function getTotalCost(){
+    debugger;
     const foodCost = getInputValue('food');
     const rentCost = getInputValue('rent');
     const clothesCost = getInputValue('clothes');
-    
-    const totalCosts = foodCost + rentCost + clothesCost;
-    return totalCosts;
+
+    if(isNaN(foodCost) == true){
+        errorMessage.innerText = "Please enter a Number";
+        return;
+    }
+    else if(isNaN(rentCost) == true){
+        errorMessage.innerText = "Please enter a Number";
+        return;
+    }
+    else if(isNaN(clothesCost) == true){
+        errorMessage.innerText = "Please enter a Number";
+        return;
+    }
+    else if(foodCost < 0){
+        errorMessage.innerText = "Please enter a Positive Number"
+        return;
+    }
+    else if(rentCost < 0){
+        errorMessage.innerText = "Please enter a Positive Number"
+        return;
+    }
+    else if(clothesCost < 0){
+        errorMessage.innerText = "Please enter a Positive Number"
+        return;
+    }
+    else{
+        errorMessage.innerText = "";
+        const totalCosts = foodCost + rentCost + clothesCost;
+        return totalCosts;
+    }
  }
+
+ 
 
     // calculate button 
 
 document.getElementById('calculate-button').addEventListener('click', function(){  
+    
     const incomeAmount = getInputValue('income');
     const totalCost = getTotalCost();
 
-    //available balance
+    if(isNaN(incomeAmount) == true){
+        errorMessage.innerText = "Please enter a Number";
+        return;
+    }
+    else if(incomeAmount < 0){
+        errorMessage.innerText = "Please enter a Positive Number"
+        return;
+    }
+    else if(isNaN(totalCost) == true){
+        errorMessage.innerText = "Please enter a Number";
+        return;
+    }
+    else if(totalCost > incomeAmount){
+        errorMessage.innerText = "You have not much money to expense";
+        return;
+    }
+    else{
+        errorMessage.innerText = '';
+        //available balance
     const balance = incomeAmount - totalCost;
 
     // set Expenses 
     const totalExpenses = getOutputValue('total-expenses', totalCost);
     
     // set balances
-    const availableBalance = getOutputValue('balance', balance);    
+    const availableBalance = getOutputValue('balance', balance); 
+    }    
 })
 
 
@@ -53,7 +104,17 @@ document.getElementById('save-button').addEventListener('click', function(){
     // reamaining balance 
     const reamainingBalance = incomeAmount - (totalCosts + savingAmount);
 
-    // set remaining balance
+   if(reamainingBalance < 0){
+        errorMessage.innerText = "You cannot save money due to income is less than expense"
+        return;
+   }
+   else if(isNaN(saveInput) == true){
+        errorMessage.innerText = "Please enter a Number"
+        return;
+   }
+   else{
+       // set remaining balance
     getOutputValue('remaining-balance', reamainingBalance);
+   }
 })
 
